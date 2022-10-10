49c9e520e3b31573a050bc9cbc6088a948b1290c
prince9871 <soniprince142@gmail.com>
2022-10-10 17:08:01 +0530

    register api done

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const route=require('./route/route')
+
+const app=express()
+
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://prince9871:BZjeaWxY1uTLCefz@cluster0.pelsn1m.mongodb.net/group60Database',{
+    useNewUrlParser:true
+})
+    .then(()=>console.log("mongoDb connected"))
+    .catch((err)=>err)
+
+app.use('/',route)
+
+app.use(function(req,res){
+    return res.status(400).send("invalid url")
+})
+
+const prt=3000
+app.listen(prt,function(){
+    console.log('Express running on port '+prt)
+})
