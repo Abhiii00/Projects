58728988d8a72c3c08038c02270af24196f904c7
Abhiii00 <abhaythakur79753@gmail.com>
2022-10-11 11:07:48 +0530

    get api in progress

--- src/route/route.js
+++ src/route/route.js
@@ -3,5 +3,6 @@ const router=express.Router()
 const userController=require('../controllers/userController')
 
 router.post('/register',userController.userCreate)
+router.get('/user/:userId/profile',userController.getUserDetails)
 
 module.exports=router
