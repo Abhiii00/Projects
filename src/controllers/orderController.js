49d578922b5643f2854e07844e5687454126111d
Abhiii00 <abhaythakur79753@gmail.com>
2022-10-17 17:00:52 +0530

    cart, get and dlt completed

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -9,10 +9,14 @@ const cartModel = require('../models/cartModel')
 const createOrder = async function(req, res){
     try {
         let userId = req.params.userId
-        let {cartId} = req.body
         if(!v.isValidObjectId(userId)) return res.status(400).send({ status: false, msg: `${userId} is not valid userid`})
         let findUserData = await userModel.findById({ _id: userId })
         if (!findUserData) return res.status(404).send({ status: false, msg: `no user found by this ${userId}`})
+
+        let cartData = req.body
+        let {cartId} = cartData
+        if(req.body.length == 0)  return res.status(400).send({ status: false, message: "Please give some data to create an order" })
+        if(!cartId) return res.status(400).send({ status: false, message: "cartId is required" })
         if(!v.isValidObjectId(cartId)) return res.status(400).send({ status: false, msg: `${cartId} is not valid cartId`})
         let findCartData = await cartModel.findById({ _id: cartId })
         if (!findCartData) return res.status(404).send({ status: false, msg: `no data found by this cartId ${cartId}`})
